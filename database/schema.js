db63fd01704607d4fec96a2acaff4bb62aedab01
Ethan Lipkind <Ethan.lipkind@gmail.com>
2018-01-25 15:16:40 -0500

    database set up

--- database/schema.js
+++ database/schema.js
@@ -1,9 +1,9 @@
 var knex = require('./config.js').knex;
 var bookshelf = require('bookshelf')(knex);
 
-bookshelf.knex.schema.hasTable('Users').then((exists) => {
+bookshelf.knex.schema.hasTable('Users').then(function(exists) {
 	if ( !exists ) {
-		bookshelf.knex.schema.createTable('Users', (e) => {
+		bookshelf.knex.schema.createTable('Users', function(e) {
 			e.increments('id').primary();
 			e.text('username').notNullable();
 			e.text('password').notNullable();
@@ -11,9 +11,11 @@ bookshelf.knex.schema.hasTable('Users').then((exists) => {
 			e.text('email'); // optional
 			e.text('type').notNullable(); // options: 'client' or 'trainer'
 			e.text('profile_data', 'longtext').nullable();
+		}).then(function(a) {
+			console.log('created the users table!');
 		});
 	}
-});
+})
 
 bookshelf.knex.schema.hasTable('Exercise_Plans').then((exists) => {
 	if ( !exists ) {
@@ -21,10 +23,12 @@ bookshelf.knex.schema.hasTable('Exercise_Plans').then((exists) => {
 			e.increments('id').primary();
 			e.text('name').notNullable(); // e.g. "Ethan's plan"
 			e.text('regimen', 'longtext').notNullable(); // stringified object
-			e.integer('trainer_id').notNullable();
-			e.integer('client_id').notNullable();
+			e.integer('trainer_id').notNullable().unsigned();
+			e.integer('client_id').notNullable().unsigned();
 			e.foreign('trainer_id').references('Users.id');
 			e.foreign('client_id').references('Users.id');
+		}).then(function(a) {
+			console.log('created the exercise_plans table!');
 		});
 	}
 });
@@ -35,10 +39,12 @@ bookshelf.knex.schema.hasTable('Diet_Plans').then((exists) => {
 			e.increments('id').primary();
 			e.text('name').notNullable(); // e.g. "Ethan's plan"
 			e.text('diet', 'longtext').notNullable(); // stringified object
-			e.integer('trainer_id').notNullable(); 
-			e.integer('client_id').notNullable();
+			e.integer('trainer_id').notNullable().unsigned(); 
+			e.integer('client_id').notNullable().unsigned();
 			e.foreign('trainer_id').references('Users.id');
 			e.foreign('client_id').references('Users.id');
+		}).then(function(a) {
+			console.log('created the diet_plans table!');
 		});
 	}
 });
@@ -47,11 +53,13 @@ bookshelf.knex.schema.hasTable('Spotters').then((exists) => {
 	if ( !exists ) {
 		bookshelf.knex.schema.createTable('Spotters', (e) => {
 			e.increments('id').primary();
-			e.integer('trainer_id').notNullable();
-			e.integer('client_id').notNullable();
+			e.integer('trainer_id').notNullable().unsigned();
+			e.integer('client_id').notNullable().unsigned();
 			e.text('type').notNullable(); // ex., 'nutritionist'
 			e.foreign('trainer_id').references('Users.id');
 			e.foreign('client_id').references('Users.id');
+		}).then(function(a) {
+			console.log('created the spotters table!');
 		});
 	}
 });
@@ -60,10 +68,12 @@ bookshelf.knex.schema.hasTable('Chatrooms').then((exists) => {
 	if ( !exists ) {
 		bookshelf.knex.schema.createTable('Chatrooms', (chatrooms) => {
 			chatrooms.increments('id').primary();
-			chatrooms.integer('room_id').notNullable(); // Firebase room id
-			chatrooms.integer('user_id').notNullable(); // foreign key
+			chatrooms.integer('room_id').notNullable().unsigned(); // Firebase room id
+			chatrooms.integer('user_id').notNullable().unsigned(); // foreign key
 			chatrooms.foreign('user_id').references('Users.id');
-    });
+    }).then(function(a) {
+			console.log('created the chatrooms table!');
+		});
   }
 });
 
@@ -71,10 +81,12 @@ bookshelf.knex.schema.hasTable('Daily_Records').then((exists) => {
 	if ( !exists ) {
 		bookshelf.knex.schema.createTable('Daily_Records', (record) => {
 			record.increments('id').primary();
-			record.integer('user_id').notNullable(); // foreign key
+			record.integer('user_id').notNullable().unsigned(); // foreign key
 			record.text('data', 'longtext').notNullable(); // all user inputs
 			record.foreign('user_id').references('Users.id');
-    });
+    }).then(function(a) {
+			console.log('created the daily_records table!');
+		});
   }
 });
 
@@ -82,10 +94,12 @@ bookshelf.knex.schema.hasTable('Personal_Records').then((exists) => {
 	if ( !exists ) {
 		bookshelf.knex.schema.createTable('Personal_Records', (record) => {
       record.increments('id').primary();
-			record.integer('user_id').notNullable(); // foreign key
+			record.integer('user_id').notNullable().unsigned(); // foreign key
 			record.text('data', 'longtext').notNullable(); // all PRs
 			record.foreign('user_id').references('Users.id');
-    });
+    }).then(function(a) {
+			console.log('created the personal_records table!');
+		});
   }
 });
 
