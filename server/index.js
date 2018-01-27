819ea638865e5668ca39fad582b81634c912b6f0
Limitless <limitless@Limitlesss-MacBook-Pro.local>
2018-01-27 10:18:36 -0500

    merge conflicts

--- server/index.js
+++ server/index.js
@@ -2,18 +2,11 @@ const express = require('express');
 const db = require('../database/schema.js');
 const models = require('../database/models.js')
 const admin = require('firebase-admin');
-const graphqltools = require('graphql-tools');
+// const firebase = require('firebase');
 const app = express();
 const port = 4000;
 const TOKEN = require('./../TOKENS.js');
-const serviceAccount = require("./../serviceAccount.json");
-const body_parser = require('body-parser');
-const collections = require('../database/collections.js'); // db functions live in here
-const graphQLSchema = require('../database/graphQLSchema.js');
-const resolvers = require('../database/resolvers.js');
-const graphqlHTTP = require('express-graphql');
-const graphql = require('graphql');
-
+var serviceAccount = require("./../serviceAccount.json");
 
 var firebaseDb = admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -21,20 +14,19 @@ var firebaseDb = admin.initializeApp({
 });
 
 var database = firebaseDb.database();
+const body_parser = require('body-parser');
+const collections = require('../database/collections.js'); // db functions live in here
+const graphQLSchema = require('../database/graphQLSchema.js');
 
-app.use('/graphql', graphqlHTTP({
-	schema: graphqltools.makeExecutableSchema({typeDefs: graphQLSchema, resolvers: resolvers}),
-	graphiql: true,
-	context: collections
-}));
-
-function pushMessge(roomId, message){ //ADD THIS MESSAGE TO THIS ROOM
-    database.ref('/rooms/' + roomId + '/messages').push(message)
-}
+app.post('/message', (req, res)=>{
+    function pushMessge(roomId, message){ //ADD THIS MESSAGE TO THIS ROOM CHANGE VARS ROOMID & MESSAGES
+        database.ref('/rooms/' + roomId + '/messages').push(message)
+    }
+})
 
 function roomsListenerSetup(roomsArray){ //ADDS EVENTLISTENER FOR NEW MESSAGES TO THESE ROOMS
     roomsArray.map((roomId)=>{
-        database.ref('/rooms/' + roomId + '/messages').on("value", function(snapshot){
+        database.ref('/rooms/' + "testRoom2" + '/messages').on("child_added", function(snapshot){
             console.log(snapshot.val());
         }, 
         function(errorObject){
@@ -42,5 +34,5 @@ function roomsListenerSetup(roomsArray){ //ADDS EVENTLISTENER FOR NEW MESSAGES T
         })
     })
 }
-    
+
 app.listen(port, () => console.log('Listening on port', port));
