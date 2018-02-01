b08d8f7e4c8ab4c5a8410069a6a81ae3840916b2
Limitless <limitless@Limitlesss-MacBook-Pro.local>
2018-01-31 20:27:00 -0500

    chatroom func

--- jabroni/Chat.js
+++ jabroni/Chat.js
@@ -1,9 +1,40 @@
 import React from 'react'
-import { View, Text, TextInput, StyleSheet, AsyncStorage } from 'react-native'
+import { View, Text, TextInput, StyleSheet, TouchableHighlight, AsyncStorage } from 'react-native'
 import { graphql, ApolloProvider, compose } from 'react-apollo';
 import gql from 'graphql-tag';
+import { Button, Divider } from 'react-native-elements'
+
+import * as firebase from 'firebase';
+import TOKENS from './../TOKENS.js';
+var t = require('tcomb-form-native');
+var Form = t.form.Form;
+
+const firebaseConfig = {
+  apiKey: TOKENS.firebaseConfig.apiKey,
+  authDomain: TOKENS.firebaseConfig.authDomain,
+  databaseURL: TOKENS.firebaseConfig.databaseURL,
+  projectId: TOKENS.firebaseConfig.projectId,
+  storageBucket: TOKENS.firebaseConfig.storageBucket,
+  messagingSenderId: TOKENS.firebaseConfig.messagingSenderId,
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
 
 class Chat extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      showRoom : "none",
+      showRooms : "flex",
+      messages : [],
+       Person: t.struct({
+          Message: t.String,
+        }),
+    }
+    this.touch = this.touch.bind(this)
+  }
   // componentDidMount() {
   //   const { nav } = this.props;
 
@@ -12,6 +43,16 @@ class Chat extends React.Component {
   //   // });
 
   // }
+  touch(room_id){
+    database.ref('/rooms/' + room_id + '/messages').on("value", (snapshot)=>{
+      this.setState({messages: Object.values(snapshot.val())}, ()=>{
+        this.setState({showRooms: "none", showRoom: "flex"})
+      })
+    }, 
+    function(errorObject){
+        console.log("the read failed: " + errorObject.code);
+    })
+  }
 
   render() {
     if(this.props.data.loading){
@@ -19,14 +60,35 @@ class Chat extends React.Component {
     }
     return (
       <View style={styles.container}>
-        <View style={{marginTop: 50}}>
+
+        <View style={{marginTop: 50, display: this.state.showRooms}}>
           {/* <Text> Maybe this could be our chat? <Text>{console.log(">>>+++>>>",this.props.data.getChatRooms[0])}</Text></Text> */}
-          {this.props.data.getChatRooms.map( (room)=>{
+          {this.props.data.getChatRooms.map( (room, idx)=>{
             console.log(room)
             if(room.user.fullName){
-              <Text>room.user.fullName</Text>
+              return <Button
+              key={idx}
+              title={room.user.fullName} 
+              onPress={
+                
+                ()=>{this.touch(room.room_id)}
+
+              }
+              />
             }
           } )}
+        </View>
+        <View style={{display: this.state.showRoom}}>
+          {this.state.messages.map( (message, idx)=>{
+            return <Text key={idx} style={styles.message}> {message.user} :     {message.message}</Text>
+          } )}
+          <Form
+            ref="form"
+            type={this.state.Person}
+          />
+          <TouchableHighlight style={styles.button} onPress={()=>{console.log("press")}} underlayColor='red'>
+            <Text style={styles.buttonText}>Send</Text>
+          </TouchableHighlight>
         </View>
       </View>
     );
@@ -38,6 +100,27 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     flex: 1,
   },
+  message: {
+    borderColor: "red",
+    borderWidth: 2,
+    padding: 5
+  },
+  button: {
+    height: 36,
+    backgroundColor: '#48BBEC',
+    borderColor: '#48BBEC',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  },
+  buttonText: {
+    fontSize: 18,
+    color: 'white',
+    alignSelf: 'center'
+  }
+  
 })
 
 const getChatRooms = gql`
