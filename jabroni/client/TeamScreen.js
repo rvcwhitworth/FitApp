60b72abfa0798109c3a2e5355dddfd3c2cd6e755
rvcwhitworth <rvcwhitworth@gmail.com>
2018-02-02 14:50:15 -0500

    Add addSpotter mutation

--- jabroni/client/TeamScreen.js
+++ jabroni/client/TeamScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Alert, StyleSheet, Image, View, Text, Dimensions, Button, Keyboard } from 'react-native';
+import { AsyncStorage, Alert, StyleSheet, Image, View, Text, Dimensions, Button, Keyboard } from 'react-native';
 import { SearchBar } from 'react-native-elements';
 import { graphql, ApolloProvider, withApollo } from 'react-apollo';
 import gql from 'graphql-tag';
@@ -13,31 +13,59 @@ class TeamScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      search: false,
       searchTerm: "",
       searchResults: [],
       loading: false,
-      photos: {}
+      photos: {},
+      spotters: []
     }
     this.search = this.search.bind(this);
     this.addSpotter = this.addSpotter.bind(this);
   }
 
-  addSpotter(id) {
-    Alert.alert('+ clicked', 'user id:' + id);
+  componentDidMount () {
+    AsyncStorage.getItem('@FitApp:UserInfo')
+    .then((userInfoString) => {
+      this.state.user = JSON.parse(userInfoString);
+      this.props.client.query({
+        query: spotterQuery,
+        variables: {
+          id: this.state.user.id
+        }
+      }).then(({data}) => {
+        this.setState({spotters: data.getSpotters})
+      })
+    })
+  }
+
+  addSpotter(user) {
+    this.props.client.mutate({
+      mutation: addSpotter,
+      variables: {
+        client_id: this.state.user.id,
+        trainer_id: user.id
+      }
+    }).then(({data}) => {
+      console.log('DATA AFTER SET', data)
+      let searchResults = this.state.searchResults;
+      searchResults.splice(searchResults.indexOf(user));
+      this.setState({
+        spotters: [...this.state.spotters, data.setSpotter],
+        searchResults
+      })
+    })
   }
 
   search() {
     // send this.state.searchTerm to graphQL query
-    this.setState({loading: true, photos: {}, searchResults: []}, () => {
+    this.setState({loading: true, searchResults: []}, () => {
       this.props.client.query({
-        query: q,
+        query: searchQuery,
         variables: {
           fullName: this.state.searchTerm
         }
       }).then((results) => {
         // do something with the results
-        console.log('response from search: ', results.data);
         let temp = this.state.searchResults;
         results.data.getUsersByFullName.forEach((person) => {
           temp.push(person);
@@ -76,72 +104,57 @@ class TeamScreen extends React.Component {
 
   render(){
   	const { width, height } = Dimensions.get('window');
+    return (
+    <View style={{alignContent: 'center', flexDirection: 'column', width:width, height:height, backgroundColor: 'white'}}>
+      <Text style={styles.header}> Your Team </Text>
 
-    return this.state.search ? (
-    <View style={{flexDirection: 'column', width:width, height:height, backgroundColor: 'white'}}>
-      <View style={{flex: 1, marginTop: 5}}>
-        <View style={{flex:1, flexDirection: 'row', justifyContent: 'center'}}>
-          <SearchBar lightTheme style={{height: 40, width: width, margin: 10}} clearIcon={{color: '#86939e', name: 'close'}} 
-            placeholder="find spotters" onChangeText={(text) => this.setState({searchTerm: text})} 
-            onClearText={() => this.setState({searchTerm: ''})} ref={searchBar => this.searchBar = searchBar}
-          />
-          <Button onPress={this.search} title="search" />
-        </View>
-      </View>
-      <View style={{flex: 8, justifyContent: 'center'}}>
-        {this.state.loading ? <Text style={{textAlign: 'center', fontSize: 16}}> Searching for spotters! </Text> :
-        this.state.searchResults.map((user) => {
-          return (
-            <View style={{flexDirection: 'row', width: width, margin: 10}}>
-              {this.state.photos[user.id] ? 
-                (<Image source={{ uri: `data:image/jpg;base64,${this.state.photos[user.id]}` }} style={styles.circle}/>) :
-                (<Image source={{ uri: ''}}/>)}
-              <View style={{flex : 1}}>
-                <Text>Username: {user.username}</Text>
-                <Text>Name: {user.fullName}</Text>
-              </View>
-              <Button title={'+'} onPress={() => this.addSpotter(user.id)}/>
+      {/*spotters */}
+      <View style={{flex: 4, alignContent: 'center'}}>
+        {this.state.spotters.map((user) => {
+          <View key={user.id} style={{alignContent: 'center', justifyContent: 'space-around', alignContent: 'center', flexDirection: 'row', width: width  * (2/3), margin: 10}}>
+            {this.state.photos[user.id] ? 
+              (<Image source={{ uri: `data:image/jpg;base64,${this.state.photos[user.id]}` }} style={styles.circle}/>) :
+              (<Image source={require('../../images/tearingMeApart.jpeg')} style={styles.circle}/>)}
+            <View style={{flex : 1, marginLeft: 5}}>
+              <Text>Username: <Text style={{fontWeight: 'bold'}}>{user.username}</Text></Text>
+              <Text>Name: <Text style={{fontWeight: 'bold'}}>{user.fullName}</Text></Text>
             </View>
-          )
+          </View>
         })}
-        <Chat nav={this.props.nav}/>
       </View>
-      <FooterNav nav={this.props.nav} index={4} />
-    </View>) :
-    (<View style={{flexDirection: 'column', width:width, height:height, backgroundColor: 'white'}}>
-    <View style={{flex: 1, marginTop: 5}}>
-      <View style={{flex:1, flexDirection: 'row', justifyContent: 'center'}}>
-        <SearchBar lightTheme style={{height: 40, width: width, margin: 10}} clearIcon={{color: '#86939e', name: 'close'}} 
+
+      <Text style={styles.subHeader}> Add a Spotter </Text>
+      <View style={{width: width, flex:1, flexDirection: 'row', justifyContent: 'center'}}>
+        <SearchBar lightTheme style={{height: 40, margin: 10}} clearIcon={{color: '#86939e', name: 'close'}} 
           placeholder="find spotters" onChangeText={(text) => this.setState({searchTerm: text})} 
           onClearText={() => this.setState({searchTerm: ''})} ref={searchBar => this.searchBar = searchBar}
         />
         <Button onPress={this.search} title="search" />
       </View>
-    </View>
-    <View style={{flex: 8, justifyContent: 'center'}}>
-      {this.state.loading ? <Text style={{textAlign: 'center', fontSize: 16}}> Searching for spotters! </Text> :
-      this.state.searchResults.map((user) => {
-        return (
-          <View style={{flexDirection: 'row', width: width, margin: 10}}>
-            {this.state.photos[user.id] ? 
-              (<Image source={{ uri: `data:image/jpg;base64,${this.state.photos[user.id]}` }} style={styles.circle}/>) :
-              (<Image source={{ uri: ''}}/>)}
-            <View style={{flex : 1}}>
-              <Text>Username: {user.username}</Text>
-              <Text>Name: {user.fullName}</Text>
+      <View style={{flex: 4, alignContent: 'center', justifyContent: 'flex-start'}}>
+        {this.state.loading ? <Text style={{textAlign: 'center', fontSize: 16}}> Searching for spotters! </Text> :
+        this.state.searchResults.map((user) => {
+          return (
+            <View key={user.id} style={{alignContent: 'center', justifyContent: 'space-around', alignContent: 'center', flexDirection: 'row', width: width  * (2/3), margin: 10}}>
+              {this.state.photos[user.id] ? 
+                (<Image source={{ uri: `data:image/jpg;base64,${this.state.photos[user.id]}` }} style={styles.circle}/>) :
+                (<Image source={require('../../images/tearingMeApart.jpeg')} style={styles.circle}/>)}
+              <View style={{flex : 1, marginLeft: 5}}>
+                <Text>Username: <Text style={{fontWeight: 'bold'}}>{user.username}</Text></Text>
+                <Text>Name: <Text style={{fontWeight: 'bold'}}>{user.fullName}</Text></Text>
+              </View>
+              <Button title={'  +  '} onPress={() => this.addSpotter(user)}/>
             </View>
-            <Button title={'+'} onPress={() => this.addSpotter(user.id)}/>
-          </View>
-        )
-      })}
-      <Chat nav={this.props.nav}/>
-    </View>
+          )
+        })}
+        <Chat nav={this.props.nav}/>
+      </View>
     <FooterNav nav={this.props.nav} index={4} />
   </View>);
   }
 }
 
-const q = gql`
+const searchQuery = gql`
   query getUsersByFullName($fullName: String!){
     getUsersByFullName(fullName: $fullName) {
       id
@@ -152,12 +165,45 @@ const q = gql`
     }
   }
 `
+const spotterQuery = gql`
+query getSpotters($id: Int!){
+  getSpotters(id: $id, type: "client") {
+    client {
+      username
+      fullName
+    }
+  }
+}
+`
+
+const addSpotter = gql`
+mutation setSpotter($trainer_id: Int!, $client_id: Int!){
+  setSpotter(trainer_id: $trainer_id, client_id: $client_id, type: "support") {
+    id
+    fullName
+    email
+    profile_data
+    username
+  }
+}
+`
+
 
 const styles = StyleSheet.create({
   circle: {
     height: 50,
     width: 50,
     borderRadius: 50/2
+  },
+  header : {
+    textAlign: 'center',
+    fontSize: 28,
+    fontWeight: 'bold'
+  },
+  subHeader : {
+    textAlign: 'center',
+    fontSize: 22,
+    fontWeight: 'bold'
   }
 })
 
