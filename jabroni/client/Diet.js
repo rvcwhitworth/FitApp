631ab38b5fa589d08e97511bfced72472d1e8d6b
rvcwhitworth <rvcwhitworth@gmail.com>
2018-02-02 12:08:42 -0500

    Add photos to spotter search

--- jabroni/client/Diet.js
+++ jabroni/client/Diet.js
@@ -84,8 +84,7 @@ class DietScreen extends React.Component {
       this.props.client.query({
         query: dietQuery,
         variables: {
-          id: this.state.user.id,
-          type: this.state.user.type
+          id: this.state.user.id
         }
       })
       .then(({data}) => {
@@ -246,8 +245,8 @@ const styles = StyleSheet.create({
 })
 
 const dietQuery = gql`
-query getDietPlans($id: Int!, $type: String!){
-  getDietPlans(id: $id, type: $type) {
+query getDietPlans($id: Int!){
+  getDietPlans(id: $id, type: "client") {
     diet
     trainer {
       fullName
