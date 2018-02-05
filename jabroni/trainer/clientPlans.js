e7c279c826cbb49e0aa67854006b10c1c7ba7efc
Jason McCutchan <jasonmccutchan@Jasons-MacBook-Pro.local>
2018-02-05 18:54:44 -0500

    Trainer roster

---
+++ jabroni/trainer/clientPlans.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import { View, Text, TextInput, StyleSheet, TouchableOpacity, Button, Dimensions, AsyncStorage } from 'react-native'
+import NavClient from './rosterClientNav.js'
+
+const { width, height } = Dimensions.get('window');
+
+
+
+  // async componentDidMount() {
+  //   await AsyncStorage.setItem('key' : 'I like to save it.')
+  //   //JSON stringify the data into storage and JSON parse it out, THIS IS SO AMAZING
+  // }
+
+class ClientPlans extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state ={
+      client: 'Loading'
+    }
+  }
+
+
+  async componentDidMount() {
+    this.props.nav.cleanUp()
+    let name = await AsyncStorage.getItem('@FitApp:SelectedClient')
+    console.log('what is name Jon???', name)
+    this.setState({
+      client: JSON.parse(name).name
+    })
+  }
+
+  async componentWillReceiveProps(nextProps){
+    let name = await AsyncStorage.getItem('@FitApp:SelectedClient')
+    if(name){
+      this.setState({
+        client: JSON.parse(name).name
+      })
+    }
+  }
+
+  componentWillUnmount() {
+    this.props.nav.cleanUp()
+  }
+
+  render() {
+    return (
+      <View style={{flexDirection:'column', width:width, height:height, backgroundColor: 'white'}}>  
+      <View style={{flex:1}}>
+      </View>  
+        <View style={{flex: 2}}>
+          <Text style={{fontSize: 30, marginBottom: 50, textAlign:'center'}}>You are looking at {this.state.client} client workout plans</Text>
+        </View>
+        <NavClient nav={this.props.nav} index={3}/>
+      </View>
+    );
+
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    width: width,
+    height: height,
+    backgroundColor: 'white'
+  },
+
+  nav: {
+    position: 'absolute',
+    bottom:0,
+  }
+})
+
+export default ClientPlans
--- jabroni/trainer/trainerRoster.js
+++ jabroni/trainer/trainerRoster.js
@@ -1,29 +1,115 @@
 import React from 'react'
-import { View, Text, TextInput, StyleSheet, Dimensions } from 'react-native'
+import { View, Text, TextInput, StyleSheet, Dimensions, TouchableOpacity, Image, Button, AsyncStorage } from 'react-native'
 import Chat from '../utilities/chatIcon'
 import Nav from './trainerNav.js'
+import VertNav from './rosterClientNav.js'
 const { width, height } = Dimensions.get('window');
 
 class TrainerRoster extends React.Component {
-  componentDidMount() {
-    const { nav } = this.props;
-
-    // nav.onNavigateShouldAllow(() => {
-    //    return true;
-    // });
+  constructor(props) {
+    super(props)
+    this.state ={
+      clients: ['Ethan', 'Chris', 'Ryan', 'Jason'],
+      selected: false,
+      selectedUser: ''
+    }
+    this.selectClient = this.selectClient.bind(this)
+    this.backToRoster = this.backToRoster.bind(this)
 
   }
 
+  componentDidMount(){
+    this.props.nav.onNavigateShouldAllow(() => {
+      console.log('whattt is the state.', this.state.selected)
+      if(!this.state.selected){
+       return false
+     } else{
+      return true
+     }
+    });
+
+    // this.props.nav.onNavigateDownShouldAllow(() => {
+    //   if(this.state.selected){
+    //     console.log('swiping')
+    //     return true
+    //   } else{
+    //     return false
+    //   }
+    // })
+    // this.props.nav.onNavigateLeftShouldAllow(() => {
+    //   if(this.state.selected){
+    //     console.log('swiping')
+    //     return true
+    //   } else{
+    //     return false
+    //   }
+    // })
+    // this.props.nav.onNavigateRightShouldAllow(() => {
+    //   if(this.state.selected){
+    //     console.log('swiping')
+    //     return true
+    //   } else{
+    //     return false
+    //   }
+    // })
+    // this.props.nav.onNavigateUpShouldAllow(() => {
+    //   if(this.state.selected){
+    //     console.log('swiping')
+    //     return true
+    //   } else{
+    //     return false
+    //   }
+    // })
+
+  }
+
+  selectClient(name){
+    var payload = {
+      name: name
+    }
+    AsyncStorage.setItem('@FitApp:SelectedClient', JSON.stringify(payload))
+    this.props.nav.navigate('ClientBodyComp')
+  }
+
+  backToRoster(){
+    this.setState({
+      selected:false,
+      selectedUser: ''
+    })
+  }
+
   render() {
+    //Need to query in component did mount or async storage the trainers clientel
+    //map through array of those guys and their pictures as thumbnails?
+    if(!this.state.selected){
     return (
       <View style={styles.container}>
-        <View style={{flex:1}}>
+        <View style={{flex:1, flexDirection:'column'}}>
+        {this.state.clients.map((val, key) => {
+          return(<View key={key} style={{flex:1, flexDirection:'column'}}>
+            <TouchableOpacity onPress={()=>{this.selectClient(val)}}>
+            <Image source={require('../../images/muscle.gif')} style={{width:40, height: 40}} />
+            <Text> {val} </Text>
+            </TouchableOpacity>
+            </View>
+            )
+        })}
           <Chat nav={this.props.nav} />
-          <Text> This is where we will put the roster at for the trainer to view and make each person a touchable component to render </Text>
         </View>
         <Nav nav={this.props.nav} index={2}/>
       </View>
-    )
+    )} else{
+      return(
+        <View style={styles.container}>
+        <View style={{flex:1}}>
+        <Text>{this.state.selectedUser}</Text>
+        </View>
+        <Button title='Go Back' onPress={()=>this.backToRoster()}/>
+        <VertNav nav={this.props.nav} />
+        </View>
+        )
+      //render selectedUser with subcomponent
+    }
 
   }
 }
@@ -33,8 +119,15 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     flexDirection: 'column',
     width:width, 
-    height:height
+    height:height,
+    flex:1,
+    paddingTop: 40
   },
+    button: {
+    alignItems: 'center',
+    backgroundColor: 'red',
+    padding: 10
+  }
 })
 
 export default TrainerRoster
