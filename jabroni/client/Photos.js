892314382fe9ea5f069ec253dd69ead1c5e8333d
Ethan Lipkind <Ethan.lipkind@gmail.com>
2018-02-09 19:36:17 -0500

    Add styling to photo gallery

--- jabroni/client/Photos.js
+++ jabroni/client/Photos.js
@@ -8,15 +8,113 @@ import {
 	StyleSheet,
 	Button,
 	Alert,
-	TouchableHighlight
+	TouchableHighlight,
+	TouchableOpacity
 } from "react-native";
 import NavFooter from "./FooterNav.js";
 import Chat from "../utilities/chatIcon.js";
 import firebase from "../utilities/firebase.js";
 var moment = require("moment");
 
+const trash = require("../../images/trash.png");
+
 const imageStore = firebase.storage();
 const database = firebase.database();
+
+const width = Dimensions.get("window").width;
+const height = Dimensions.get("window").height;
+
+const styles = StyleSheet.create({
+	galleryContainer: {
+		width: width,
+		height: height-60,
+		backgroundColor: "black",
+		flexDirection: "column"
+	},
+	loadingMessageContainer: {
+		alignSelf: 'center',
+	},
+	loadingMessage: {
+		color: "white",
+		fontSize: 20,
+		fontWeight: "bold"
+	},
+	imageContainer: {
+		flex: 3,
+		width: "100%",
+		height: "100%"
+	},
+	image: {
+		resizeMode: "contain",
+		width: "100%",
+		height: "100%"
+	},
+	prevContainer: {
+		top: height * 0.45,
+		width: 20,
+		left: 0,
+		flexDirection: "row",
+		position: "absolute",
+		// justifyContent: "space-between",
+		zIndex: 2
+	},
+	nextContainer: {
+		top: height * 0.45,
+		width: 20,
+		left: width-(width * 0.20),
+		flexDirection: "row",
+		position: "absolute",
+		// justifyContent: "space-between",
+		zIndex: 2
+	},
+	prev: {
+		// alignSelf: "flex-start",
+		width: width * 0.20,
+		height: height * 0.20
+	},
+	next: {
+		// alignSelf: "flex-end",
+		width: width * 0.20,
+		height: height * 0.20
+	},
+	prevButton: {
+		resizeMode: 'contain',
+		width: "100%",
+		height: "100%"
+	},
+	nextButton: {
+		resizeMode: 'contain',
+		width: "100%",
+		height: "100%"
+	},
+	buttonContainer: {
+		flex: 2,
+		zIndex: 2,
+		flexDirection: "row",
+		width: width
+		// justifyContent: "space-between",
+		// alignSelf: 'flex-end'
+	},
+	setProfPicButton: {
+		alignSelf: 'center',
+		marginTop: 5
+	},
+	trashContainer: {
+		width: width * 0.05,
+		height: height * 0.05,
+		backgroundColor: 'black',
+		borderRadius: (width * 0.05 / 2),
+		alignSelf: 'flex-start',
+		marginTop: 5,
+		marginLeft: 5
+	},
+	trash: {
+		width: '100%',
+		height: '100%',
+		tintColor: 'white',
+		backgroundColor: 'black'
+	}
+});
 
 class Photos extends React.Component {
 	constructor(props) {
@@ -59,7 +157,7 @@ class Photos extends React.Component {
 									.getDownloadURL()
 									.then(url => {
 										this.downloadPic(url, name, fileNames.length);
-									})
+									});
 							});
 						});
 				});
@@ -77,9 +175,9 @@ class Photos extends React.Component {
 				return a[0].localeCompare(b[0]) * -1; // newest photo should appear first
 			});
 			this.setState({ photos: photos }, () => {
-				if ( this.state.photos.length === length ) {
-					this.setState({loading: false});
-				} 
+				if (this.state.photos.length === length) {
+					this.setState({ loading: false });
+				}
 			});
 		};
 		xhr.open("GET", url);
@@ -102,7 +200,7 @@ class Photos extends React.Component {
 
 	toggleButtons(e) {
 		e.preventDefault();
-		this.setState({showButtons: !this.state.showButtons});
+		this.setState({ showButtons: !this.state.showButtons });
 	}
 
 	delete(e) {
@@ -156,66 +254,59 @@ class Photos extends React.Component {
 		const { width, height } = Dimensions.get("window");
 		const curr = this.state.photos[this.state.index]; // a tuple representing the [timestamp,image] currently being displayed
 		return (
-			<View style={styles.galleryContainer}>
-				{this.state.loading ? <View style={styles.galleryContainer}><Text>Loading!</Text></View> : this.state.photos.length === 0 ? (
-					<View style={styles.galleryContainer}>
-						<Text>No photos to display.</Text>
-					</View>
-				) : (
-					<View>
-					{this.state.showButtons ? 
-					<View style={styles.buttonContainer}>
-						<Button onPress={this.prev} title="prev" />
-						<Button
-							onPress={this.setProfPic}
-							title="set profile picture"
-						/>
-						<Button onPress={this.next} title="next" />
-						<Button onPress={this.delete} title="delete" />
-					</View> : null}
-					<View style={{ width: "100%", height: "100%"}}>
-						<TouchableHighlight style={styles.imageContainer} onPress={this.toggleButtons}>
-							<Image source={{ uri: `data:image/jpg;base64,${curr[1]}` }} style={styles.image} />
-						</TouchableHighlight>
-					</View>
+			<View style={{"backgroundColor":"black", flexDirection: 'column', width: width, height: height}}>
+				<View style={styles.galleryContainer}>
+					{this.state.loading ? (
+						<View style={styles.loadingMessageContainer}>
+							<Text style={styles.loadingMessage}>Loading!</Text>
+						</View>
+					) : this.state.photos.length === 0 ? (
+						<View style={styles.galleryContainer}>
+							<Text>No photos to display.</Text>
+						</View>
+					) : (
+						<View>
+							<View style={styles.prevContainer}>
+								<TouchableOpacity style={styles.prev} onPress={this.prev}>
+									<Image source={require("../../images/prev.png")} style={styles.prevButton}/>
+								</TouchableOpacity>
+							</View>
+							<View style={styles.nextContainer}>
+								<TouchableOpacity style={styles.next} onPress={this.next}>
+									<Image source={require("../../images/next.png")} style={styles.nextButton} />
+								</TouchableOpacity>
+							</View>
+							{this.state.showButtons ? (
+								<View style={styles.buttonContainer}>
+									<TouchableOpacity style={styles.trashContainer} onPress={this.delete}>
+										<Image source={trash} style={styles.trash} />
+									</TouchableOpacity>
+									<Button
+										style= {styles.setProfPicButton}
+										onPress={this.setProfPic}
+										title="set profile picture"
+									/>
+								</View>
+							) : null}
+							<View style={{ width: "100%", height: "100%" }}>
+								<TouchableHighlight
+									style={styles.imageContainer}
+									onPress={this.toggleButtons}
+								>
+									<Image
+										source={{ uri: `data:image/jpg;base64,${curr[1]}` }}
+										style={styles.image}
+									/>
+								</TouchableHighlight>
+							</View>
+						</View>
+					)}
+					<Chat nav={this.props.nav} TopNav={this.props.topNav} />
 				</View>
-				)}
-
-				<Button onPress={this.next} title="next" />
-				<Chat nav={this.props.nav} TopNav={this.props.topNav}/>
-
 				<NavFooter nav={this.props.nav} index={3} />
 			</View>
-		)
+		);
 	}
 }
-
-const styles = StyleSheet.create({
-	galleryContainer: {
-		width: Dimensions.get("window").width,
-		height: Dimensions.get("window").height-50,
-		backgroundColor: "white",
-		flexDirection: "column"
-	},
-	imageContainer: {
-		flex: 3,
-		width: "100%",
-		height: "100%"
-	},
-	textContainer: {
-
-	},
-	buttonContainer: {
-		flex: 2,
-		zIndex: 2,
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-	},
-	image: {
-		resizeMode: "contain",
-		width: "100%",
-		height: "100%"
-	}
-});
 
 export default Photos;
