c5ea7e5ecd118f354596a11842ede7c3e70043ce
Ethan Lipkind <Ethan.lipkind@gmail.com>
2018-02-13 20:52:42 -0500

    photos coming in on web client

--- webAPI/src/components/Client/Photos.jsx
+++ webAPI/src/components/Client/Photos.jsx
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import axios from 'axios';
 import Photo from './Photo.jsx';
+import {Grid} from 'semantic-ui-react';
 
 class Photos extends React.Component{
   constructor(props){
@@ -11,30 +12,33 @@ class Photos extends React.Component{
       this.state = {
         id : this.props.id || null,
         username : this.props.user || null,
-        loading: true,
-        photos: []
+        photos: [] // [[URL1, timestamp1],[URL2, timestamp2], ...]
       }
   }
 
   componentDidMount() {
-    this.props.photos.forEach((key) => {
-      console.log('key to download: ', key);
-      axios.get('https://fitpics.s3.amazonaws.com/public/' + key).then(({data}) => {
-        // 
-        console.log('base64 for key ' + key + ': ', data);
-      }).catch((err) => {
-        console.log('s3 error: ', err);
-      })
+    this.setState({photos: this.props.photos}, () => {
+      console.log('set photos state: ', this.state.photos);
     });
+    // this.props.photos.forEach((key) => {
+    //   console.log('key to download: ', key);
+    //   // axios.get('https://fitpics.s3.amazonaws.com/public/' + key).then(({data}) => {
+    //   //   // 
+    //   //   console.log('base64 for key ' + key + ': ', data);
+    //   // }).catch((err) => {
+    //   //   console.log('s3 error: ', err);
+    //   // })
+    // });
   }
 
   render() {
-    return this.state.loading ? <div>Loading...</div> :
-      <div>
+    return (
+      <Grid columns={3}>
         {this.state.photos.map((photo, i) => {
-          return <Photo key={i} />
+          return <Photo key={i} url={photo[0]} timestamp={photo[1]} />
         })}
-      </div>
+      </Grid>
+    );
       
   }
 }
