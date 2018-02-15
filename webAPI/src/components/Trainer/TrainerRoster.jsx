db1cc341d530e90290ba4f47310f1f313e7d9b65
Ethan Lipkind <Ethan.lipkind@gmail.com>
2018-02-14 19:52:27 -0500

    setup progress photo functions

--- webAPI/src/components/Trainer/TrainerRoster.jsx
+++ webAPI/src/components/Trainer/TrainerRoster.jsx
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import ChangeUser from '../../actions/example.jsx'
 import { Grid, Image, Button } from 'semantic-ui-react'
-
+const s3 = require('../../utilities/s3_utilities.js');
 
 class Roster extends React.Component{
   constructor(props){
@@ -25,6 +25,44 @@ class Roster extends React.Component{
     console.log('i can click')
   }
 
+  grabProfilePic(client) {
+    if ( client.profile_data && JSON.parse(client.profile_data).profilePictureURL ) {
+      return JSON.parse(client.profile_data).profilePictureURL;
+    } else {
+      return undefined;
+    }
+  }
+
+  grabPhotos(client) {
+    let fixedList = [];
+
+    s3.getPhotosList(client.id).then((list) => {
+      console.log('list: ', list);
+      let l = _.pluck(list, 'key');
+      console.log('l is: ', l);
+      l.splice(l.indexOf(client.id+'/'), 1);
+      console.log('now l is: ', l);
+      l.forEach(url => {
+        // get rid of the id/ at beginning of string
+        let t = url.split('/');
+
+        // splice out and store the timestamp at end of string separately
+        let v = t[1].split('TIMESTAMP=');
+        console.log('timestamp: ', v[1]);
+
+        // replace the single quote with a forward slash
+        let s = v[0].split("'").join('/');
+
+        let u = "http://res.cloudinary.com/dvhehr6k8/image/upload/" + s;
+
+        fixedList.push([u, v[1]]);
+      });
+    }).then(() => {
+      // do something with fixedList to display current client's progress photos
+      // this.setState({})
+    })
+  }
+
   render() {
     var point = Math.ceil(this.state.roster.length/2)
     var firstHalf = this.state.roster.slice(0, point)
@@ -35,6 +73,7 @@ class Roster extends React.Component{
         <Grid columns={2} divided>
     <Grid.Column style={{textAlign:'center'}}>
     {firstHalf.map((val, key) => {
+      console.log('profile url: ', this.grabProfilePic(val.client));
         return (<Grid.Row style={{padding:'20px'}}>
           <Button fluid primary onClick={this.selectClient} >{val.client.fullName}</Button>
           </Grid.Row>
